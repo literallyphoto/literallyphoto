e2e57736049c9b64fb577b4320041e066208d4b8
literallyphoto <34273734+literallyphoto@users.noreply.github.com>
2017-12-22 13:09:29 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -106,7 +106,25 @@ $(".fancybox").fancybox({
     }
 });
 
+var myform = $("form#myform");
+myform.submit(function(event){
+	event.preventDefault();
 
+  // Change to your service ID, or keep using the default service
+  var service_id = "default_service";
+  var template_id = "literally_photo_feedback";
+
+  myform.find("button").text("Отправка...");
+  emailjs.sendForm(service_id,template_id,"myform")
+  	.then(function(){ 
+    	alert("Sent!");
+       myform.find("button").text("Отправлено");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("button").text("Отправлено");
+    });
+  return false;
+});
 
 
 
